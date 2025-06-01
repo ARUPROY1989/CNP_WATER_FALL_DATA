f3e8776d6df363e4b0c6d9d803a798d54b606f23
Mandar Majumder <mandarmajumder@gmail.com>
2025-06-01 15:24:30 +0530

    css updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,22 @@
-<div class="container mx-auto ">
+<div class="container w-screen">
   <!-- Hero Section -->
-  <section class="bg-gray-500 text-white p-16 rounded-lg">
-    <h1 class="text-4xl md:text-5xl font-bold mb-4">India Waterfall Database</h1>
-    <p class="text-lg md:text-xl">Dive into the Wonders of Indian Waterfalls</p>
+  <section class="bg-gray-500 text-white p-16 w-screen h-screen flex flex-col-reverse">
+    
+    <p class="text-lg md:text-3xl mb-4 font-sans">Dive into the Wonders of Indian Waterfalls</p>
+    <h1 class="text-4xl md:text-9xl  mb-4 font-sans">Database</h1>
+    <h1 class="text-4xl md:text-9xl  mb-4 font-sans">India Waterfall </h1>
+
   </section>
 
   <!-- Search Section -->
-  <section class="">
-    <h2 class="text-xl font-semibold mb-4">Search the Waterfall Database</h2>
-    <div class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
-      <div class="relative w-full md:w-1/2">
-        <input type="text" placeholder="Search by" class="w-full p-2 border border-gray-300 rounded">
+  <section class="p-16 w-screen min-h-screen flex items-center flex-col">
+    <h2 class="text-xl font-semibold mb-4 w-max">Search the Waterfall Database</h2>
+    <div class="flex flex-col w-full md:w-1/2 space-y-4 md:space-y-0 md:space-x-4">
+      <div class="relative w-full">
+        <input type="text" placeholder="Search by" class="w-full p-2 border border-gray-300 ">
         <span class="absolute right-2 top-2">▼</span>
       </div>
-      <div class="flex space-x-2">
+      <div class="flex space-x-2 self-start">
         <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Filter</button>
         <button class="border border-blue-500 text-blue-500 px-4 py-2 rounded hover:bg-blue-100">Add Filter</button>
       </div>
