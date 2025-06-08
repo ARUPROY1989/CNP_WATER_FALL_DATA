bf3b342892bdc644a481f7800ebbcd6b51ff8693
Mandar Majumder <mandarmajumder@gmail.com>
2025-06-08 10:12:48 +0530

    bg immage added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
+<script>
+  import banner from '$lib/images/banner.jpg'; 
+</script>
 <div class="container w-screen">
   <!-- Hero Section -->
-  <section class="bg-gray-500 text-white p-16 w-screen h-screen flex flex-col-reverse">
-    
+  <section class=" text-white p-16 w-screen min-h-screen flex flex-col-reverse" style="background-image: url({banner}); background-size: cover; background-position: center;">
     <p class="text-lg md:text-3xl mb-4 font-sans">Dive into the Wonders of Indian Waterfalls</p>
     <h1 class="text-4xl md:text-9xl  mb-4 font-sans">Database</h1>
     <h1 class="text-4xl md:text-9xl  mb-4 font-sans">India Waterfall </h1>
-
+    
   </section>
 
   <!-- Search Section -->
-  <section class="p-16 w-screen min-h-screen flex items-center flex-col">
+  <section class="p-16 w-screen min-h-screen flex items-center flex-col" >
     <h2 class="text-xl font-semibold mb-4 w-max">Search the Waterfall Database</h2>
     <div class="flex flex-col w-full md:w-1/2 space-y-4 md:space-y-0 md:space-x-4">
       <div class="relative w-full">
